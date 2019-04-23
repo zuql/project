688f029df4d9007dbb03d778626fda5338663523
zuql <17712743419@163.com>
2019-04-23 18:35:24 +0800

    同步数据

---
+++ springboot-mvc-mybatis-shiro/src/main/java/com/zuql/project/springbootmvcmybatisshiro/SpringbootMvcMybatisShiroApplication.java
@@ -0,0 +1,13 @@
+package com.zuql.project.springbootmvcmybatisshiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcMybatisShiroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMvcMybatisShiroApplication.class, args);
+    }
+
+}
---
+++ springboot-mvc-mybatis-shiro/src/test/java/com/zuql/project/springbootmvcmybatisshiro/SpringbootMvcMybatisShiroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zuql.project.springbootmvcmybatisshiro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootMvcMybatisShiroApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
